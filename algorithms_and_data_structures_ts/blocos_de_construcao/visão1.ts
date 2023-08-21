00c7f1e763c0295b2557157a750ac55a78abb8f3
Eduardo270704 <luis270704@gmail.com>
2023-08-21 19:51:13 -0300

    algorithms_and_data_structures_ts

---
+++ algorithms_and_data_structures_ts/blocos_de_construcao/visão1.ts
@@ -0,0 +1,18 @@
+import { somar, subtrair, despedir } from "./modelo";
+let escolha: string
+escolha = "somar"
+let resultado: number
+switch (escolha) {
+    case "somar":
+        resultado = somar(1, 2);
+        console.log(resultado)
+        break
+    case "subtrair":
+        resultado = subtrair(1, 2);
+        console.log(resultado)
+        break
+    case "despedir":
+        console.log(despedir())
+        break
+}
+
