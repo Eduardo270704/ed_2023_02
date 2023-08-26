0547a98bc783e1233322a9bfc7c5876ecdb96d15
Eduardo270704 <luis270704@gmail.com>
2023-08-25 22:21:53 -0300

    exercicio1.11_lesm

--- Unid2/exercicio1.11_lesm/src/part2_lesm.ts
+++ Unid2/exercicio1.11_lesm/src/part2_lesm.ts
@@ -4,17 +4,17 @@ const prompt = prompt_sync();
 let n1_lesm:number = parseInt(prompt("Digite o primeiro número:"));
 let n2_lesm:number = parseInt(prompt("Digite o segundo número:"));
 
-let resultado:string = ""; 
+let resultado_lesm:string = ""; 
 
 switch (true){
     case n1_lesm > n2_lesm:
-        resultado = "O primeiro número é o maior.";
+        resultado_lesm = "O primeiro número é o maior.";
         break;
 
     case n1_lesm <= n2_lesm:
-        resultado = "O primeiro número é menor ou igual ao segundo.";
+        resultado_lesm = "O primeiro número é menor ou igual ao segundo.";
         break;
 }
-console.log (resultado);
+console.log (resultado_lesm);
 
 export {};
